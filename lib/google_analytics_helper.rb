b60a01c04e69fb9d1210f8ef2aa3c7c8b73d127a
Ashley McKenzie <ash@ashmckenzie.org>
2009-12-05 14:34:41 +1100

    - removed README and replaced with README.textile - added ga_generate_non_async_js() method for non async style js

--- lib/google_analytics_helper.rb
+++ lib/google_analytics_helper.rb
@@ -1,7 +1,7 @@
 module GoogleAnalyticsHelper
   def ga_generate_js(ua)
     return false if ua.nil?
-    return "
+    return <<EOF
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', '#{ua}']);
 _gaq.push(['_trackPageview']);
@@ -12,6 +12,18 @@ _gaq.push(['_trackPageview']);
       'http://www') + '.google-analytics.com/ga.js';
   ga.setAttribute('async', 'true');
   document.documentElement.firstChild.appendChild(ga);
-})();"
+})();
+EOF
+  end
+  def ga_generate_non_async_js(ua)
+    return false if ua.nil?
+    return <<EOF
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+try {
+var pageTracker = _gat._getTracker("#{ua}");
+pageTracker._trackPageview();
+} catch(err) {}
+EOF
   end
 end
